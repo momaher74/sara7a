8acb6e06d3782e4f7dd4d17a373e63ec03777670
momaher74 <84906867+momaher74@users.noreply.github.com>
2024-01-26 22:11:33 +0200

    Update user_routes.js
    s

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {signin , signup, verifayEmail} = require('../contollers/user_controllers');
 const {signinValidation, signupValidation} = require('../middleware/validation'); 
 
-router.post('/signup', signupValidation, signup); 
+router.post('/signup',  signup); 
 router.post('/signin',signinValidation,signin);
 router.get('/verify/:token', verifayEmail)
 
